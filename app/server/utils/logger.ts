8b85f4c6ac5dd6b8ffa965aa84b7df6e96ff3e28
yashjoshiordex <155522014+yashjoshiordex@users.noreply.github.com>
2025-06-12 11:07:40 +0530

    update

--- app/server/utils/logger.ts
+++ app/server/utils/logger.ts
@@ -1,39 +1,31 @@
 import winston from "winston";
-import { format } from "winston";
+import { format } from 'winston';
 import 'winston-daily-rotate-file';
 
-const logDir = true ? '/tmp/logs' : 'logs';
-
-const customFormat = format.printf(({ timestamp, level, message, shop }) => {
-    return `${timestamp} [${level}]: shop:[${shop}] ${message}`;
+var transport = new (winston.transports.DailyRotateFile)({
+    filename: '/tmp/logs/DonateMe-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
 });
 
-const transports: winston.transport[] = [];
-
-if (false) {
-    // Use console logging in Vercel to avoid file system issues
-    transports.push(new winston.transports.Console());
-} else {
-    // Local file logging
-    const fileRotateTransport = new winston.transports.DailyRotateFile({
-        filename: `${logDir}/DonateMe-%DATE%.log`,
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d'
-    });
-
-    transports.push(fileRotateTransport);
-}
+const customFormat = format.printf(({ timestamp, level, message, shop }) => {
+    return `${timestamp} [${level}]: shop:[${shop}] ${message} `;
+});
 
 export const logger = winston.createLogger({
-    level: 'info',
     format: format.combine(
         format.timestamp(),
         customFormat
     ),
-    transports,
-    exceptionHandlers: isVercel
-        ? [new winston.transports.Console()]
-        : [new winston.transports.File({ filename: `${logDir}/exceptions.log`, level: 'error' })],
+    transports: [
+        transport
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({
+            filename: 'logs/exceptions.log',
+            level: 'error'
+        })
+    ]
 });
